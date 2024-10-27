01f4b10d17b86b57af02927559347b1cd8b89427
Miyagoshi_Sota <k23122kk@aitech.ac.jp>
2024-10-27 15:06:34 +0900

    change:text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <a href="https://github.com/SystemEngineeringTeam/kurodenwa_flutter_app" >あぷり</a>
         <a href="https://github.com/SystemEngineeringTeam/kurodenwa_arduino">あるでぃーの</a>
         <a href="https://github.com/SystemEngineeringTeam/kurodenwa_raspberrypi">らずぱい</a>
-        <a href="https://www.sysken.net/">次サークル</a>
+        <a href="https://www.sysken.net/">自分達のサークル</a>
       </header>
     </div>
   );
